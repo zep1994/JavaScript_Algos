30b52ad62061b8485e08055f0ea36559f0a793f9
zep1994 <snaxbags@protonmail.com>
2019-11-29 19:58:32 -0500

    add notes for finishing

--- OrderedWords/index.js
+++ OrderedWords/index.js
@@ -33,3 +33,9 @@ function remove_number(word) {
 }
 
 order("is2 Thi1s T4est 3a")
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
